f965776ee0fbab6192dfafb5d347e4de73fccb10
lindsay777 <lindsaytseng777@gmail.com>
2017-08-03 10:57:37 +0800

    orders

--- server.js
+++ server.js
@@ -293,10 +293,10 @@ function new_order (req, res, next) {
 						console.log(result);
 
 						save(req, res, next);
-						console.log(chalk.magentaBright('Order %s is NEWED in BC!'), data.order_id);
+						console.log(chalk.magentaBright('order %s is NEWED in BC!'), data.order_id);
 					}
 					else if(result.args.check == false)
-						print_color(chalk.magentaBright('Order Unavailable'))
+						print_color(chalk.magentaBright('order Unavailable'))
 				}
 			}
 		});
@@ -321,7 +321,7 @@ function new_order (req, res, next) {
 				res.send(err.message);
 				return next();
 			}
-			console.log(chalk.magentaBright('Order %s is NEWED in DB!'), data.order_id);
+			console.log(chalk.magentaBright('order %s is NEWED in DB!'), data.order_id);
 			// res.send('%s has been added to the DB!', data.name);
 			// res.send(data.order_id);
 		});
@@ -354,11 +354,15 @@ function update_order (req, res, next) {
 		'date': req.body.date,
 		'room_type': req.body.room_type
 	}
+
+	// 從order_id切一切拿到舊的key
 	var fields = data.order_id.split('_');
 	data.old_key = fields[0] + '_' + fields[1];
 	data.new_key = data.date + '_' + data.room_type;
 
 	console.log(chalk.cyan('user_id: %s, new_key: %s'), data.user_id, data.new_key);
+
+	event_filter(req, res, next);
 
 	// BC
 	web3.personal.unlockAccount(web3.eth.coinbase, 'internintern', 300);	//解鎖要執行 function 的 account
@@ -379,53 +383,78 @@ function update_order (req, res, next) {
 		}
 	);
 
-	// DB
-	// update order-
-	Order.findOneAndUpdate({ order_id: data.order_id }, { key: data.new_key, user_id: data.user_id, date: data.date, room_type: data.room_type, order_id: data.order_id }, function(err, user) {
-		if (err) {
-			console.log(err);
-			res.send(err.message);
-			return next();
-		}else if( data.order_id == null ){
-			console.log('cannot find order_id');
-			res.send('cannot find order_id');
-			return next();
-		}else{
-			// we have the updated user returned to us
-			console.log(chalk.magentaBright('Order %s is UPDATED in DB!'), data.order_id);
-			res.send(data.order_id);
-		}
-	});
-	// change room: old key soldout -1, new key soldout +1
-	Room.findOneAndUpdate({ key: data.old_key },
-		{
-      		$inc: {
-        		soldout: -1
-      		} 
-      	},
-  		function(err, room) {
-			if (err) {
-				console.log(err);
-				res.send(err.message);
-				return next();
-		}
-	});
-	Room.findOneAndUpdate({ key: data.new_key },
-		{
-      		$inc: {
-        		soldout: 1
-      		} 
-      	},
-  		function(err, room) {
-			if (err) {
-				console.log(err);
-				res.send(err.message);
-				return next();
-		}
-	});
+	//必須確認 監聽這個訂單有沒有拿到(true false) 回傳false代表沒有這個
+	function event_filter (req, res, next) {
+		var update_order_event = myContract.update_order_event({},{
+			toBlock: 'latest'
+		});
 
-	console.log('room %s soldout -1, room %s soldout +1', data.old_key, data.new_key);
-	return next();
+		var block = web3.eth.getBlock('latest').number;
+		update_order_event.watch(function(error, result){
+			if (!error && result.blockNumber > block){
+				if(web3.toAscii(result.args.new_key) == data.new_key){
+					if(result.args.check == true){
+						update_order_event.stopWatching();
+						console.log(result);
+
+						save(req, res, next);
+						console.log(chalk.magentaBright('order %s is UPDATED in BC!'), data.order_id);
+					}
+					else if(result.args.check == false)
+						console.log(chalk.magentaBright('order Unavailable'))
+				}
+			}
+		});
+	}
+
+	// DB
+	// update order
+	function save(req, res, next){
+		Order.findOneAndUpdate({ order_id: data.order_id }, { key: data.new_key, user_id: data.user_id, date: data.date, room_type: data.room_type, order_id: data.order_id }, function(err, user) {
+			if (err) {
+				console.log(err);
+				res.send(err.message);
+				return next();
+			}else if( data.order_id == null ){
+				console.log('cannot find order_id');
+				res.send('cannot find order_id');
+				return next();
+			}else{
+				// we have the updated user returned to us
+				console.log(chalk.magentaBright('order %s is UPDATED in DB!'), data.order_id);
+				res.send(data.order_id);
+			}
+		});
+		// change room: old key soldout -1, new key soldout +1
+		Room.findOneAndUpdate({ key: data.old_key },
+			{
+	      		$inc: {
+	        		soldout: -1
+	      		} 
+	      	},
+	  		function(err, room) {
+				if (err) {
+					console.log(err);
+					res.send(err.message);
+					return next();
+			}
+		});
+		Room.findOneAndUpdate({ key: data.new_key },
+			{
+	      		$inc: {
+	        		soldout: 1
+	      		} 
+	      	},
+	  		function(err, room) {
+				if (err) {
+					console.log(err);
+					res.send(err.message);
+					return next();
+			}
+		});
+		console.log(chalk.cyan('room %s soldout -1, room %s soldout +1'), data.old_key, data.new_key);
+		return next();
+	}
 };
 
 function delete_order (req, res, next) {
@@ -463,8 +492,8 @@ function delete_order (req, res, next) {
 			return next();
 		} else {
 			// we have deleted the user
-			console.log(chalk.green(data.order_id + 'has been deleted!'));
-			res.send(data.order_id + 'has been deleted!');
+			console.log(chalk.magentaBright('order: %s is DELETED'), data.order_id);
+			res.send(data.order_id);
 		}
 	});
 	// change room soldout
@@ -481,7 +510,7 @@ function delete_order (req, res, next) {
 			return next();
 		}
 
-		console.log('room.key: %s updated!',data.key);
+		console.log(chalk.cyan('room: %s soldout -1!'),data.key);
 		return next();
 	});
 };
