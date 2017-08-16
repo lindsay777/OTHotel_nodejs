78ea1b0a5145051aea30e9843b65dc377713cbcd
lindsay777 <lindsaytseng777@gmail.com>
2017-08-16 15:52:57 +0800

    update api

--- db.js
+++ db.js
@@ -20,23 +20,37 @@ var orderSchema = new mongoose.Schema({
 	user_id: { type: String, required: true },
 	date: { type: String, required: true },
 	room_type: { type: Number, required: true },
-	order_id: { type: String, required: true, unique: true }
+	order_id: { type: String, required: true, unique: true },
+	price: { type: Number, required: true }
 });
 
 var roomSchema = new mongoose.Schema({
 	key: { type: String, required: true, unique: true },
-	total: Number,
-	soldout: { type: Number, default: 0 }
-})
+	total: { type: Number, required: true },
+	soldout: { type: Number, default: 0 },
+	original_price: { type: Number, required: true },
+	sell_price: { type: Number, required: true },
+	state: { type: Boolean, required: true , default: true},
+	promotion_id: { type: Number, required: true, default: 0 },
+	isHoliday: { type: Boolean, required: true, default: false},
+	isEarly: { type: Boolean, required: true, default: false}
+});
+
+var promotionSchema = new mongoose.Schema({
+	id: { type: Number, required: true, unique: true },
+	price: { type: Number, required: true, default: 0 }
+});
 
 orderSchema.plugin(uniqueValidator);
 roomSchema.plugin(uniqueValidator);
+promotionSchema.plugin(uniqueValidator);
 
 // we need to create a model using it
 var Order = mongoose.model('Order', orderSchema);
 var Room = mongoose.model('Room', roomSchema);
+var Promotion = mongoose.model('Promotion', promotionSchema);
 
 // make this available to our users in our Node applications
 module.exports = {
-	Order, Room
+	Order, Room, Promotion
 }
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const myContract = Web3.myContractInstance;
 const DB = require('./db');
 const Order = DB.Order;
 const Room = DB.Room;
+const Promotion = DB.Promotion;
 
 // chalk
 const chalk = require('chalk');
@@ -28,34 +29,130 @@ server.use(
   }
 );
 
-// user
+// use
 server.use(restifyPlugin.bodyParser());
+server.use(restifyPlugin.queryParser());
+
+// promotion function
+server.post('/post/promotion', new_promotion);
+server.post('/update/promotion', update_promotion);
+server.post('/delete/promotion', delete_promotion);
 
 // room function
 server.get('/get/room/all_room', find_all_room);
 server.get('/get/room', find_room);
+server.get('/get/room/price', get_price);
 server.post('/post/room', new_room);
 server.post('/post/multiroom', multiroom);
+server.post('/post/discount', set_discount);
 server.post('/update/room', update_room);
 server.post('/delete/room', delete_room);
-
 server.listen(8070, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
 
 // order function
-// server.get('/echo/:name', name);
 server.get('/get/order/all_order', find_all_order);
 server.get('/get/order', find_order);
 server.post('/post/order', new_order);
 server.post('/update/order', update_order);
-// server.post('/delete/all_user', delete_all_user);
 server.post('/delete/order', delete_order);
 
-// function name (req, res, next) {
-// 	res.send(req.params.name);
-// 	return next();
-// };
+// PROMOTION
+function new_promotion (req, res, next) {
+
+	var data = {
+		'id': req.body.id,
+		'price': req.body.price
+	}
+
+	// create a new user called user ENTITY
+	var promotion_data = new Promotion({
+		id: data.id,
+		price: data.price
+	});
+	console.log(chalk.cyan('id: %s, price: %s'), data.id, data.price);
+	
+	web3.personal.unlockAccount(web3.eth.coinbase, 'internintern', 300);	//解鎖要執行 function 的 account
+	myContract.new_promotion(	// transfer 是 contract 裡 的一個 function
+		data.id, data.price,
+
+		{
+			from: web3.eth.coinbase,	//從哪個ethereum帳戶執行
+			'gas': myContract.new_promotion.estimateGas(data.id, data.price) //執行function所需的gas ((發現放input突然就可以了
+		},
+		function(err, result) {	//callback 的 function
+			if (!err){
+				console.log("Transaction_Hash: " + result);
+			}
+			else {
+				console.log(err);
+			}
+		}
+	);
+
+	// call the built-in save method to save to the database
+	promotion_data.save(function(err) {
+		if (err) {
+			console.log(err);
+			res.send(err.message);
+			return next();
+		}
+		console.log(chalk.cyan('Promotion %s is NEWED!'), data.id);
+		// res.send('%s has been added to the DB!', data.name);
+		res.send(data.id + ' is NEWED!');
+		return next();
+	});
+};
+
+function update_promotion (req, res, next) {
+
+	var data = {
+		'id': req.body.id,
+		'price': req.body.price
+	}
+	console.log(chalk.cyan('id: %s, price: %s'), data.id, data.price);
+
+	// find the room and update
+	Promotion.findOneAndUpdate({ id: data.id }, { id: data.id, price: data.price}, function(err, user) {
+		if (err) {
+			console.log(err);
+			res.send(err.message);
+			return next();
+		}else if( data.id == null ){
+			console.log(chalk.red('cannot find id'));
+			res.send('cannot find id');
+			return next();
+		}else{
+			// we have the updated user returned to us
+			console.log(chalk.cyan('Promotion %s is UPDATED!'), data.id);
+			res.send(data.id + ' is UPDATED!');
+			return next();
+		}
+	});
+};
+
+function delete_promotion (req, res, next) {
+
+	var data = {
+		'id': req.body.id,
+	}
+
+	// find the room and remove
+	Promotion.findOneAndRemove({ id: data.id }, function(err) {
+		if (err) {
+			console.log(err);
+			res.send(err.message);
+			return next();
+		} else {
+			// we have deleted the user
+			console.log(chalk.cyan('Promotion %s is DELETED!'), data.id);
+			res.send(data.id + ' is DELETED!');
+			return next();
+		}
+		
+	});
+};
 
 // ROOM
 function find_all_room (req, res, next) {
@@ -104,23 +201,35 @@ function new_room (req, res, next) {
 
 	var data = {
 		'key': req.body.key,
-		'total': req.body.total
+		'total': req.body.total,
+		'original_price': req.body.original_price,
+		'sell_price': req.body.sell_price,
+		'promotion_id':req.body.promotion_id,
+		'isHoliday': req.body.isHoliday,
+		'isEarly': req.body.isEarly
 	}
 
 	// create a new user called user ENTITY
 	var room_data = new Room({
 		key: data.key,
-		total: data.total
+		total: data.total,
+		original_price: data.original_price,
+		sell_price: data.sell_price,
+		promotion_id: data.promotion_id,
+		isHoliday: data.isHoliday,
+		isEarly: data.isEarly
 	});
 	console.log(chalk.cyan('key: %s, total: %s, soldout: 0'), data.key, data.total);
+	console.log(chalk.cyan('original_price: %s, sell_price: %s, state: true, version: 0'), data.original_price, data.sell_price);
+	console.log(chalk.cyan('promotion_id: %s, isHoliday: %s, isEarly: %s'), data.promotion_id, data.isHoliday, data.isEarly);
 
 	web3.personal.unlockAccount(web3.eth.coinbase, 'internintern', 300);	//解鎖要執行 function 的 account
 	myContract.new_room(	// transfer 是 contract 裡 的一個 function
-		data.key, data.total, 
+		data.key, data.total, data.original_price, data.sell_price, data.promotion_id, data.isHoliday, data.isEarly,
 
 		{
 			from: web3.eth.coinbase,	//從哪個ethereum帳戶執行
-			'gas': myContract.new_room.estimateGas(data.key, data.total) //執行function所需的gas ((發現放input突然就可以了
+			'gas': myContract.new_room.estimateGas(data.key, data.total, data.original_price, data.sell_price, data.promotion_id, data.isHoliday, data.isEarly) //執行function所需的gas ((發現放input突然就可以了
 		},
 		function(err, result) {	//callback 的 function
 			if (!err){
@@ -152,7 +261,12 @@ function multiroom (req, res, next) {
 		'startdate': req.body.startdate,
 		'enddate': req.body.enddate,
 		'room_type': req.body.room_type,
-		'total': req.body.total
+		'total': req.body.total,
+		'original_price': req.body.original_price,
+		'sell_price': req.body.sell_price,
+		'promotion_id': req.body.promotion_id,
+		'isHoliday': req.body.isHoliday,
+		'isEarly': req.body.isEarly
 	}
 
 	data.startdate = new Date(data.startdate)
@@ -179,21 +293,28 @@ function multiroom (req, res, next) {
 	res.send('Rooms are NEWED!');
 	return next();
 
-	function add_room (key,total) {
+	function add_room (key, total, original_price, sell_price, promotion_id, isHoliday, isEarly) {
 		// create a new user called user ENTITY
 		var room_data = new Room({
 			key: key,
-			total: total
+			total: total,
+			original_price: original_price,
+			sell_price: sell_price,
+			promotion_id: promotion_id,
+			isHoliday: isHoliday,
+			isEarly: isEarly
 		});
 		console.log(chalk.cyan('key: %s, total: %s, soldout: 0'), room_data.key, room_data.total);
+		console.log(chalk.cyan('original_price: %s, sell_price: %s, state: true, version: 0'), room_data.original_price, room_data.sell_price);
+		console.log(chalk.cyan('promotion_id: %s, isHoliday: %s, isEarly: %s'), room_data.promotion_id, room_data.isHoliday, room_data.isEarly);
 
 		web3.personal.unlockAccount(web3.eth.coinbase, 'internintern', 300);	//解鎖要執行 function 的 account
 		myContract.new_room(	// transfer 是 contract 裡 的一個 function
-			room_data.key, room_data.total, 
+			room_data.key, room_data.total, room_data.original_price, room_data.sell_price, room_data.promotion_id, room_data.isHoliday, room_data.isEarly,
 
 			{
 				from: web3.eth.coinbase,	//從哪個ethereum帳戶執行
-				'gas': myContract.new_room.estimateGas(room_data.key, room_data.total) //執行function所需的gas ((發現放input突然就可以了
+				'gas': myContract.new_room.estimateGas(room_data.key, room_data.total, room_data.original_price, room_data.sell_price, room_data.promotion_id, room_data.isHoliday, room_data.isEarly) //執行function所需的gas ((發現放input突然就可以了
 			},
 			function(err, result) {	//callback 的 function
 				if (!err){
@@ -218,32 +339,89 @@ function multiroom (req, res, next) {
 	}
 };
 
+function set_discount (req, res, next) {
+	
+	var data = {
+		'new_discount_weekday': req.body.new_discount_weekday,
+		'new_discount_weekend': req.body.new_discount_weekend,
+		'new_discount_lastroom': req.body.new_discount_lastroom
+	}
+
+	console.log(chalk.cyan('Discount: weekday: %s, weekend: %s, lastroom: %s'), data.new_discount_weekday, data.new_discount_weekend, data.new_discount_lastroom);
+	
+	web3.personal.unlockAccount(web3.eth.coinbase, 'internintern', 300);	//解鎖要執行 function 的 account
+	myContract.set_discount(	// transfer 是 contract 裡 的一個 function
+		data.new_discount_weekday, data.new_discount_weekend, data.new_discount_lastroom,
+
+		{
+			from: web3.eth.coinbase,	//從哪個ethereum帳戶執行
+			'gas': myContract.set_discount.estimateGas(data.new_discount_weekday, data.new_discount_weekend, data.new_discount_lastroom) //執行function所需的gas ((發現放input突然就可以了
+		},
+		function(err, result) {	//callback 的 function
+			if (!err){
+				console.log("Transaction_Hash: " + result);
+				res.send('set discount success');
+				return next();
+			}
+			else {
+				console.log(err);
+				res.send('set discount failed');
+				return next();
+			}
+		}
+	);
+};
+
 function update_room (req, res, next) {
 
 	var data = {
 		'key': req.body.key,
 		'total': req.body.total,
-		'soldout': req.body.soldout		
+		'soldout': req.body.soldout,
+		'original_price': req.body.original_price,
+		'promotion_id': req.body.promotion_id,
+		'isHoliday': req.body.isHoliday
 	}
 	console.log(chalk.cyan('key: %s, total: %s, soldout: %s'), data.key, data.total, data.soldout);
+	console.log(chalk.cyan('original_price: %s, promotion_id: %s, isHoliday: %s'), data.original_price, data.promotion_id, data.isHoliday);
 
-	// find the room and update
-	Room.findOneAndUpdate({ key: data.key }, { key: data.key, total: data.total, soldout: data.soldout}, function(err, user) {
-		if (err) {
-			console.log(err);
-			res.send(err.message);
-			return next();
-		}else if( data.key == null ){
-			console.log(chalk.red('cannot find key'));
-			res.send('cannot find key');
-			return next();
-		}else{
-			// we have the updated user returned to us
-			console.log(chalk.cyan('Room %s is UPDATED!'), data.key);
-			res.send(data.key + ' is UPDATED!');
-			return next();
+	web3.personal.unlockAccount(web3.eth.coinbase, 'internintern', 300);	//解鎖要執行 function 的 account
+	myContract.edit_room(	// transfer 是 contract 裡 的一個 function
+		data.key, data.total, data.soldout, data.original_price, data.promotion_id, data.isHoliday,
+		{
+			from: web3.eth.coinbase,	//從哪個ethereum帳戶執行
+			'gas': myContract.edit_room.estimateGas(data.key, data.total, data.soldout, data.original_price, data.promotion_id, data.isHoliday)  //執行function所需的gas ((發現放input突然就可以了
+		},
+		function(err, result) {	//callback 的 function
+			if (!err){
+				console.log("Transaction_Hash: " + result);
+				update(req, res, next)
+			}
+			else {
+				console.log(err);
+			}
 		}
-	});
+	);
+
+	function update(req, res, next){
+		// find the room and update
+		Room.findOneAndUpdate({ key: data.key }, { key: data.key, total: data.total, soldout: data.soldout, original_price: data.original_price, promotion_id: data.promotion_id, isHoliday: data.isHoliday}, function(err, user) {
+			if (err) {
+				console.log(err);
+				res.send(err.message);
+				return next();
+			}else if( data.key == null ){
+				console.log(chalk.red('cannot find key'));
+				res.send('cannot find key');
+				return next();
+			}else{
+				// we have the updated user returned to us
+				console.log(chalk.cyan('Room %s is UPDATED!'), data.key);
+				res.send(data.key + ' is UPDATED!');
+				return next();
+			}
+		});
+	}
 };
 
 function delete_room (req, res, next) {
@@ -268,6 +446,50 @@ function delete_room (req, res, next) {
 	});
 };
 
+function get_price (req, res, next) {
+
+	let key = req.query.key;
+
+	let result = myContract.get_price(web3.toHex(key));
+
+	console.log(web3.toHex(key));
+
+	let state = result[0];
+	let price = result[1].c[0];
+
+	if (price == 0){
+		console.log(chalk.cyan('Cannot get price!'));
+		res.send('Cannot get price!');
+		return next();
+	} else {
+		console.log(chalk.cyan('price: %s'), price);
+		res.send(String(price));
+		return next();
+	}
+
+	if (state){
+		web3.personal.unlockAccount(web3.eth.coinbase, 'internintern', 300);	//解鎖要執行 function 的 account
+		myContract.state_change(	// transfer 是 contract 裡 的一個 function
+			key, price,
+			{
+				from: web3.eth.coinbase,	//從哪個ethereum帳戶執行
+				'gas': myContract.state_change.estimateGas(key, price) //執行function所需的gas ((發現放input突然就可以了
+			},
+			function(err, result) {	//callback 的 function
+				if (!err){
+					console.log("Transaction_Hash: " + result);
+				}
+				else {
+					console.log(err);
+				}
+			}
+		);
+		return next();
+	}else{
+		return next();
+	}
+	
+};
 
 // ORDER
 function find_all_order (req, res, next) {
@@ -321,7 +543,8 @@ function new_order (req, res, next) {
 	var data = {
 		'user_id': req.body.user_id,
 		'date': req.body.date,
-		'room_type': req.body.room_type
+		'room_type': req.body.room_type,
+		'price': req.body.price
 	};
 
 	//要新增一筆訂單
@@ -329,7 +552,7 @@ function new_order (req, res, next) {
 	console.log(data.key)
 	data.order_id = data.key + '_' + data.user_id + '_' + moment().format('HH:mm:ss');
 
-	console.log(chalk.cyan('user_id: %s, key: %s, order_id: %s'), data.user_id, data.key, data.order_id);
+	console.log(chalk.cyan('user_id: %s, key: %s, order_id: %s, price: %s'), data.user_id, data.key, data.order_id, data.price);
 
 	event_filter(req, res, next);
 
@@ -338,10 +561,10 @@ function new_order (req, res, next) {
 
 	console.timeEnd("before_transaction_time");
 	myContract.new_order(	// transfer 是 contract 裡 的一個 function
-		data.key, data.user_id, data.date, data.room_type, data.order_id,
+		data.key, data.user_id, data.date, data.room_type, data.order_id, data.price,
 		{
 			from: web3.eth.coinbase,	//從哪個ethereum帳戶執行
-			'gas': myContract.new_order.estimateGas(data.key, data.user_id, data.date, data.room_type, data.order_id) //執行function所需的gas ((發現放input突然就可以了
+			'gas': myContract.new_order.estimateGas(data.key, data.user_id, data.date, data.room_type, data.order_id, data.price) //執行function所需的gas ((發現放input突然就可以了
 		},
 		function(err, result) {	//callback 的 function
 			if (!err){
@@ -387,7 +610,8 @@ function new_order (req, res, next) {
 			user_id: data.user_id,
 			date: data.date,
 			room_type: data.room_type,
-			order_id: data.order_id
+			order_id: data.order_id,
+			price: data.price
 		});
 
 		// call the built-in save method to save to the database
@@ -429,7 +653,8 @@ function update_order (req, res, next) {
 		'order_id': req.body.order_id,
 		'user_id': req.body.user_id,
 		'date': req.body.date,
-		'room_type': req.body.room_type
+		'room_type': req.body.room_type,
+		'price': req.body.price
 	}
 
 	// 從order_id切一切拿到舊的key
@@ -437,7 +662,7 @@ function update_order (req, res, next) {
 	data.old_key = fields[0] + '_' + fields[1];
 	data.new_key = data.date + '_' + data.room_type;
 
-	console.log(chalk.cyan('user_id: %s, new_key: %s'), data.user_id, data.new_key);
+	console.log(chalk.cyan('user_id: %s, new_key: %s, price: %s'), data.user_id, data.new_key, data.price);
 
 	event_filter(req, res, next);
 
@@ -445,10 +670,10 @@ function update_order (req, res, next) {
 	web3.personal.unlockAccount(web3.eth.coinbase, 'internintern', 300);	//解鎖要執行 function 的 account
 
 	myContract.update_order(	// transfer 是 contract 裡 的一個 function
-		data.old_key, data.new_key, data.user_id, data.date, data.room_type, data.order_id,
+		data.old_key, data.new_key, data.user_id, data.date, data.room_type, data.order_id, data.price,
 		{
 			from: web3.eth.coinbase,	//從哪個ethereum帳戶執行
-			'gas': myContract.update_order.estimateGas(data.old_key, data.new_key, data.user_id, data.date, data.room_type, data.order_id) //執行function所需的gas ((發現放input突然就可以了
+			'gas': myContract.update_order.estimateGas(data.old_key, data.new_key, data.user_id, data.date, data.room_type, data.order_id, data.price) //執行function所需的gas ((發現放input突然就可以了
 		},
 		function(err, result) {	//callback 的 function
 			if (!err){
